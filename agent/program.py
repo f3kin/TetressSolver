f2b5db2863836a972d38fcf6680ea286e4250cfb
f3kin <60875fil@gmail.com>
2024-05-08 21:59:06 +1000

    Debugging, need to return a PlaceAction

--- agent/program.py
+++ agent/program.py
@@ -2,10 +2,11 @@
 # Project Part B: Game Playing Agent
 
 import queue
+from typing import Tuple, Optional
 
-MID_GAME = 75
+MID_GAME = 70
 OPENING = 3 # TODO Change this to another value
-END_GAME = 3
+END_GAME = 75
 MAX_TURN = 0
 MIN_TURN = 1
 DEPTH_VALUE = 3
@@ -80,17 +81,10 @@ class Agent:
         self.num_moves += 1
         if self.num_moves < OPENING:
             return self.book_moves[self.num_moves]
+        elif self.num_moves < END_GAME:
+            return search(self.board, self._color) 
         else:
-
-            # Current children holds all children of board to a depth of 1, placing one tile. There is 118 children, maybe right?
-            childen = search(self.board, self._color) # How do we get a placeAction from search?
-
-            # search 
-            
-            
-            
-            return 0
-
+            return endgame_search(self.board, self._color)
 
     def update(self, color: PlayerColor, action: Action, **referee: dict):
         """
@@ -98,19 +92,15 @@ class Agent:
         turn. You should use it to update the agent's internal game state. 
         """
 
-        # There is only one action type, PlaceAction
         place_action: PlaceAction = action
         c1, c2, c3, c4 = place_action.coords
         self.board.place_four_tiles(color, c1,c2,c3,c4)
-        
-        # Here we are just printing out the PlaceAction coordinates for
-        # demonstration purposes. You should replace this with your own logic
-        # to update your agent's internal game state representation.
-        print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
 
+        #print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
 
-
-
+def search(board, color):
+    # Minimax goes here
+    return minimax(board, color, 0, float('-inf'), float('inf'), True)
 
 ###### General Expansion logic (FROM EXPANSION BRANCH) ######
 
@@ -124,11 +114,6 @@ Output: A list of all possible children (after placing 1 tetromino) of a board
 Desc: Takes a board and color, finds the tiles on the board of that color, and
     performs the tile expansion from each one
 """
-
-def search(board, color):
-
-    # Minimax goes here
-    return expand(board, color)
 
 def expand(
         board: Bitboard,
@@ -145,17 +130,11 @@ def expand(
         
     # For each tile, expand it
     for index in player_tiles:
-        visited = {index}  # Add back in the set functionality
+        visited = {index}  #TODO: Implement set functionality
         all_index_placements = init_expand_from_tile(board, index, color)
         moves.extend(all_index_placements)
     
-
-    #for value in moves:
-    #    # Print all boards with move
-        
-    #    value.bitboard_display()
-    #    print("\n\n")
-    print(len(moves))
+    #print(len(moves))
     return moves
 
 
@@ -189,16 +168,9 @@ def expand_out_sexy_style(
 ):
     # Add all of the boards of depth 4 and return
     if depth == 5:
-        print(current_shape)
-        #new_board = board.copy()
-        #for i in range(4):
-        #    new_board.set_tile(current_shape[i], player_colour)
-        ##new_board.bitboard_display()
+        #print(current_shape)
         board_hash = board.get_hash()
-
-        # CHECK FOR ROW/COL CLEARNING
-
-
+        #TODO: Implement Row/col deletion
         if board_hash not in seen_hashes:
             seen_hashes.add(board_hash)
             all_shapes.append(board)
@@ -220,7 +192,6 @@ def expand_out_sexy_style(
             expand_out_sexy_style(new_board, new_index, player_colour, depth + 1, new_shape, all_shapes, seen_hashes)
 
 
-
 """
 Inputs:
     `board`
@@ -239,15 +210,89 @@ def init_expand_from_tile(
     player_colour: PlayerColor
 ) -> list[Bitboard]:
 
-    # Will have repeats, need to convert to a set
     seen_hashes = set()
     all_shapes = []
     expand_out_sexy_style(board, start_index, player_colour, 1, [start_index], all_shapes, seen_hashes)
-    #print(all_shapes)
     return all_shapes[1::]
         
-    
 
+
+
+
+###### Functions specific to Minimax #####
+
+#TODO: develop a better way of saving + storing child nodes, use acutal board type
+# class Move:
+#     def __init__(self, value:int, board:board) -> None:
+#         self.value = value
+#         self.board = board
+    
+def minimax(
+        board: Bitboard, 
+        color: PlayerColor,
+        depth: int, 
+        alpha: int, 
+        beta: int, 
+        maximizingPlayer: bool, 
+        past = {}
+    ) -> Tuple[Optional[int], Optional[PlaceAction]]:
+    if cutoff_test(board, depth):
+        return None, None 
+    #TODO: make this more efficient through hashing, won't work otherwise
+    #if board in past:
+        #return past[board]
+    if maximizingPlayer:
+        return max_value(board, color, depth, alpha, beta, past)
+    else:
+        return min_value(board, color, depth, alpha, beta, past)
+
+def max_value(board, color, depth, alpha, beta, past):
+    maxEval = float('-inf')
+    best_move = None
+    for child in expand(board, color):
+        eval_score, _ = minimax(child, color, depth+1, alpha, beta, False)
+        if eval_score is not None and eval_score > maxEval:
+            maxEval = eval_score
+            best_move = child  # Update the best move
+        alpha = max(alpha, eval_score or alpha)  # Use `alpha` if `eval_score` is None
+        if beta <= alpha:
+            break
+    #TODO: store maxEval, best_move in past
+    return maxEval, best_move
+
+def min_value(board, color, depth, alpha, beta, past):
+    minEval = float('inf')
+    best_move = None
+    for child in expand(board, color):
+        eval_score, _ = minimax(child, color, depth+1, alpha, beta, True)
+        if eval_score is not None and eval_score < minEval:
+            minEval = eval_score
+            best_move = child
+        beta = min(beta, eval_score or beta)  # Use `beta` if `eval_score` is None
+        if beta <= alpha:
+            break
+    #TODO: store minEval, best_move in past
+    return minEval, best_move
+
+# Will evaluate a board state and assign it a value
+def evaluation(board, colour):
+    return 1
+
+# Checks if the move is a completed game(very unlikely), or we have reached our desired depth
+def cutoff_test(board, depth):
+    if depth > DEPTH_VALUE:
+        return True
+    else:
+        return finished(board)
+
+#Simply checks if the game is over
+def finished(board):
+    #TODO: implement me
+    return False
         
 
-    
+#returns the best move based on an end game scenario
+def endgame_search(board, color):
+    # May not be needed as we could simply modify the heuristic 
+    #TODO: Implement me
+    return search(board,color)
