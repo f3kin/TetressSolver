23f1c6eb53cfd4d5183a1556ddd1889d4cbc4219
f3kin <60875fil@gmail.com>
2024-05-13 22:53:15 +1000

    added team and project

---
+++ agent/team.py
@@ -0,0 +1,15 @@
+{
+    "team_name": "FatGPT",
+    "team_members": [
+        {
+            "name": "Finlay Ekins",
+            "student_id": "1361368",
+            "email": "fekins@student.unimelb.edu.au"
+        },
+        {
+            "name": "Nicholas Boylan",
+            "student_id": "1080729",
+            "email": "nrboylan@student.unimelb.edu.au"
+        }
+    ]
+}
