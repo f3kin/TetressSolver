25316f2b054bcbf564915b5ae8f920a8fdc04668
f3kin <60875fil@gmail.com>
2024-05-13 22:57:53 +1000

    team

--- team.py
+++ team.py
@@ -1,4 +1,16 @@
-{
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Partner Nomination
+
+# Fill in your team details here. Be sure to keep a copy of this file for use in
+# future project submissions (this is how we identify your team).
+
+metadata = {
+    # Choose a team name! Try to make it unique in some way, as it will be
+    # used to identify your team in the tournament at the end of the semester.
+    # - Allowed characters: A-Z, a-z, 0-9, -, _ (no spaces allowed)
+    # - Minimum length: 3 characters
+    # - Maximum length: 20 characters
+
     "team_name": "FatGPT",
     "team_members": [
         {
@@ -13,3 +25,4 @@
         }
     ]
 }
+
